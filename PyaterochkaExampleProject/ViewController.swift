46b0a6527f9c4c5d98e80b2226e816e03fc02700
Виктор Ковалевский <kovall89@gmail.com>
2021-10-04 13:05:54 +0300

    Initial Commit

---
+++ PyaterochkaExampleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PyaterochkaExampleProject
+//
+//  Created by Ковалевский on 04.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
