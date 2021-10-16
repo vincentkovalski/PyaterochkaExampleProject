bb7bc9c3c3a034f4228611c63c19f91262da3f6e
Виктор Ковалевский <kovall89@gmail.com>
2021-10-16 18:18:40 +0300

    Create and configure tab bar controller.

--- PyaterochkaExampleProject/Source/App/SceneDelegate.swift
+++ PyaterochkaExampleProject/Source/App/SceneDelegate.swift
@@ -17,6 +17,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+//        чтобы приложение запускалось из кода
+        window?.rootViewController = MainTabBarController()
+        window?.makeKeyAndVisible()
     }
 }
 
